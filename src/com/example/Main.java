1cd733e8a03b2d4ab956167356c3f663c6a20be7
yoziom12 <yoziom12@gmail.com>
2017-11-09 13:51:02 +0100

    Added execution of getModel and setModel methods for porsche object of Car class

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Car porsche = new Car();
         Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("Model : " + porsche.getModel());
     }
 }
