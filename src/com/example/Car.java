b4a909a3d3e4c264a2e3b7f084572867705740e9
yoziom12 <yoziom12@gmail.com>
2017-11-09 13:42:03 +0100

    Added getters and setters for Car class

--- src/com/example/Car.java
+++ src/com/example/Car.java
@@ -1,9 +1,51 @@
 package com.example;
 
 public class Car {
+
     private int doorsNumber;
     private int wheelsNumber;
     private String model;
     private String engine;
     private String colour;
+
+    public int getDoorsNumber() {
+        return doorsNumber;
+    }
+
+    public void setDoorsNumber(int doorsNumber) {
+        this.doorsNumber = doorsNumber;
+    }
+
+    public int getWheelsNumber() {
+        return wheelsNumber;
+    }
+
+    public void setWheelsNumber(int wheelsNumber) {
+        this.wheelsNumber = wheelsNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
 }
