4d6958a115dd15298913063ee7fd593079c77978
yoziom12 <yoziom12@gmail.com>
2017-11-09 13:37:58 +0100

    Added Car class, and created 2 objects of that class

---
+++ src/com/example/Car.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Car {
+    private int doorsNumber;
+    private int wheelsNumber;
+    private String model;
+    private String engine;
+    private String colour;
+}
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
     }
 }
